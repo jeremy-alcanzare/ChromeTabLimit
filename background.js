7af0b2a2e4b1ffd061efad40f9b3d59ef742268a
Crushellini <53288235+Crushellini@users.noreply.github.com>
2020-05-14 17:24:18 -0500

    added method to disable tab creation

--- background.js
+++ background.js
@@ -1,23 +1,35 @@
- chrome.browserAction.onClicked.addListener(
-	function(tab) { alert('icon clicked')});
+var currentTabNumber = 0;
+var limit = 10;
 
+//sets badge to teal
 chrome.browserAction.setBadgeBackgroundColor({ color: [59, 196, 176, 255] });
 updateTabNumber() 
-chrome.tabs.onUpdated.addListener(function(tabs)
+
+//increments badge number when button is 
+chrome.tabs.onCreated.addListener(function(tab)
+{
+if (currentTabNumber >= 10) {
+	disableTabCreation(tab)
+	}
+updateTabNumber() 
+});
+
+chrome.tabs.onRemoved.addListener(function(tab)
 {
 updateTabNumber() 
 });
 
-chrome.tabs.onRemoved.addListener(function(tabs)
-{
-updateTabNumber() 
-});
 
 function updateTabNumber() {
 	chrome.tabs.query({
     active: false,
     currentWindow: true,
 }, function(tabs) {
-chrome.browserAction.setBadgeText({text: tabs.length.toString()}); 
+chrome.browserAction.setBadgeText({text: (tabs.length + 1).toString()}); 
+currentTabNumber = tabs.length + 1;
 });
+}
+
+function disableTabCreation(tab) {
+	chrome.tabs.remove(tab.id)
 }
